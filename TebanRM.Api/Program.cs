00a90635ab19b23e0809c0904f2367cbff5c757a
eruvalca <eruvalca@outlook.com>
2023-02-28 21:25:04 -0500

    - added ef database migrations and updated

--- TebanRM.Api/Program.cs
+++ TebanRM.Api/Program.cs
@@ -1,6 +1,53 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TebanRM.Application.Identity;
+using TebanRM.Application.Models;
+using TebanRM.Application.Options;
+using TebanRM.Application.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration["DefaultConnection"]));
+
+string symmetricKey = builder.Configuration["SymmetricKey"]!;
+string issuer = builder.Configuration["Issuer"]!;
+string audience = builder.Configuration["Audience"]!;
+
+builder.Services.AddIdentity<TebanUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 8;
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedAccount = false;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthentication(auth =>
+{
+    auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        RequireExpirationTime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(symmetricKey)),
+        ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.Configure<SymmetricKeyOptions>(builder.Configuration.GetSection("SymmetricKeyOptions"));
+builder.Services.AddSingleton<SymmetricKeyService>();
+builder.Services.AddScoped<IdentityService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
